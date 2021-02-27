5785ef9c68f09fd9eab97f44537ea1aa346e650f
Di Chen <di.chen16@outlook.com>
2021-02-27 22:13:07 +0800

    go map

--- misc/trifonov/fmt.go
+++ misc/trifonov/fmt.go
@@ -12,5 +12,21 @@ func main() {
 	interpolated := fmt.Sprintf(selectStatement, myvar)
 	fmt.Println(interpolated)
 
-}
+	m := make(map[string]int)
+	m["k1"] = 7
+	m["k2"] = 13
+	fmt.Println("map:", m)
 
+	v1 := m["k1"]
+	fmt.Println("v1: ", v1)
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+}
